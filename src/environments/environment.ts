998d1ebda5c8032c92ab7ceb5a4453c07197818f
Samuelson Okoi <samuelsonokoi@gmail.com>
2020-01-01 15:18:26 +0100

    added packages and firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBnqpvlUH16ckMFnb90DMkA37ITcGmVlig",
+    authDomain: "taskmanager-f2d3b.firebaseapp.com",
+    databaseURL: "https://taskmanager-f2d3b.firebaseio.com",
+    projectId: "taskmanager-f2d3b",
+    storageBucket: "taskmanager-f2d3b.appspot.com",
+    messagingSenderId: "864817167970",
+    appId: "1:864817167970:web:c84f643a480c9a274b5b3b"
+  }
 };
 
 /*
