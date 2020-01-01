43368ff660743b3b9d557fc49d0d1368b1672727
Samuelson Okoi <samuelsonokoi@gmail.com>
2020-01-01 17:19:40 +0100

    ui modification on app.component and auth.service clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from "@angular/core";
+import {
+  FirebaseUISignInSuccessWithAuthResult,
+  FirebaseUISignInFailure
+} from "firebaseui-angular";
+import { AuthService } from "./services/auth.service";
 
 @Component({
   selector: "app-root",
@@ -7,4 +12,15 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   year = new Date().getFullYear();
+
+  constructor(private _auth: AuthService) {}
+
+  successCallback(signInSuccessData: FirebaseUISignInSuccessWithAuthResult) {
+    this._auth.saveUser(signInSuccessData.authResult.user);
+  }
+
+  errorCallback(errorData: FirebaseUISignInFailure) {
+    console.log("An error occurred");
+    console.log(errorData);
+  }
 }
